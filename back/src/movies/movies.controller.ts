46d5c37adf60495122f5039969da9b79d9cc7bd2
EliseLabarrere <lab.elise@outlook.fr>
2025-02-05 11:54:35 +0100

    Feat - Search Movie API

--- back/src/movies/movies.controller.ts
+++ back/src/movies/movies.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { MoviesService } from './movies.service';
-import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 import { AuthGuard } from '../auth/auth.guard';
 
 @ApiTags('Movies')
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) { }
-  
+
   @Get('now-playing')
   @ApiBearerAuth()
   @ApiResponse({ status: 201, description: 'Films récupérés avec succès' })
@@ -17,5 +17,29 @@ export class MoviesController {
   @UseGuards(AuthGuard)
   async getNowPlaying() {
     return this.moviesService.requestTMDB('/movie/now_playing', 'GET');
+  }
+
+  @Get('search-movie')
+  @ApiBearerAuth()
+  @ApiQuery({ name: 'query', required: true, description: 'Mot-clé de la recherche' })
+  @ApiQuery({ name: 'include_adult', required: false, description: 'Inclu des adultes' })
+  @ApiQuery({ name: 'language', required: false, description: 'Langue des résultats (ex: en, fr)' })
+  @ApiQuery({ name: 'primary_release_year', required: false, description: '' })
+  @ApiQuery({ name: 'page', required: false, description: '' })
+  @ApiQuery({ name: 'region', required: false, description: '' })
+  @ApiQuery({ name: 'year', required: false, description: '' })
+  @ApiResponse({ status: 201, description: 'Film trouvé avec succès' })
+  @ApiResponse({ status: 400, description: 'Échec : aucun film trouvé ou paramètre manquant' })
+  @ApiResponse({ status: 401, description: 'Token manquant ou non-valide' })
+  @ApiOperation({ summary: 'Recherche film' })
+  @UseGuards(AuthGuard)
+  async getSearchMovie(@Query() queryParams: Record<string, string>) {
+    const params = new URLSearchParams(queryParams).toString();
+
+    if (!queryParams.query) {
+      return { message: 'Le paramètre "query" est obligatoire' };
+    }
+
+    return this.moviesService.requestTMDB(`/search/movie?${params}`, 'GET');
   }
 }
