7da0c0d7e057437bdea949e369c33973d003b7c8
EliseLabarrere <lab.elise@outlook.fr>
2025-02-06 16:00:42 +0100

    Fix - Import Reservation Modul in app Module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movie/movie.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }),AuthModule, UsersModule, MoviesModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),AuthModule, UsersModule, MoviesModule, ReservationModule],
   controllers: [AppController],
   providers: [AppService],
 })
